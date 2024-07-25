f45cd5576ae90784e4c19d7f6647a26a56e49c55
IllusionaryFrog <mail@illusionaryfrog.com>
2024-07-25 14:22:52 +0000

    fmt

--- src/serve.rs
+++ src/serve.rs
@@ -3,10 +3,8 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(Files::new("/media", "/media").show_files_listing())
-    })
-    .bind("0.0.0.0:29839")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(Files::new("/media", "/media").show_files_listing()))
+        .bind("0.0.0.0:29839")?
+        .run()
+        .await
 }
